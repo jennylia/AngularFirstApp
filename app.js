8a2fb6416424ad727479d4b74951df70d4a5d214
jennylia <jennylian100@gmail.com>
2015-01-28 23:21:43 -0800

    change gem to array

--- app.js
+++ app.js
@@ -3,22 +3,34 @@
 		var app = angular.module('store', []); 
 		app.controller('StoreController', function(){
 
-			this.product = gem;
+			this.product = gems[0];
 
 		});
 
 		app.controller('InventoryController', function(){
 			this.inventory = inventory;
-			this.firstItem = gem;
+			this.firstItem = gems[1];
 		})
 
 
-		var gem = {
+		var gems = [
+		{
 			name: 'Dedecaheron-JL',
 			price: 2.95,
 			description: 'best gem ever by JL',
 			canPurchase: true,
-		}
+		},
+		{
+			name: 'Red Gem',
+			price: 2.95,
+			description: 'best gem ever by JL',
+			canPurchase: true,
+		},
+
+
+
+
+		];
 
 		var inventory = {
 			name: 'Closet',
